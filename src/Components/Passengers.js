0df1de5995430d1d5bddff66a9a733009ccc383b
Lucas Luzzani <lucasluzzani@gmail.com>
2023-03-24 11:18:03 -0300

    Curso de maquinas de estado en platzi

--- src/Components/Passengers.js
+++ src/Components/Passengers.js
@@ -8,14 +8,22 @@ export const Passengers = ({ state, send }) => {
     changeValue(e.target.value);
   }
 
+  const goToTicket = () => {
+    send('DONE')
+  }
+
   const submit = (e) => {
     e.preventDefault();
+    send('ADD', { newPassenger: value })
     changeValue('');
   }
+
+  const { passengers } = state.context;
 
   return (
     <form onSubmit={submit} className='Passengers'>
       <p className='Passengers-title title'>Agrega a las personas que van a volar ✈️</p>
+      {passengers.map((person, idx) => <p className='text' key={`person-${idx}`}>{person}</p>)}
       <input 
         id="name" 
         name="name" 
@@ -35,6 +43,7 @@ export const Passengers = ({ state, send }) => {
         <button
           className='Passenger-pay button'
           type="button"
+          onClick={goToTicket}
         >
           Ver mi ticket
         </button>
--- src/Containers/StepsLayout.js
+++ src/Containers/StepsLayout.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Welcome } from '../Components/Welcome';
-import { Search } from '../Components/Search';
-import { Passengers } from '../Components/Passengers';
-import { Tickets } from '../Components/Tickets';
-import './StepsLayout.css';
+import React from "react";
+import { Welcome } from "../Components/Welcome";
+import { Search } from "../Components/Search";
+import { Passengers } from "../Components/Passengers";
+import { Tickets } from "../Components/Tickets";
+import "./StepsLayout.css";
 
 export const StepsLayout = ({ state, send }) => {
   const renderContent = () => {
-    return <Welcome />;
+    if (state.matches("firstStep"))
+      return <Welcome send={send} state={state} />;
+    if (state.matches("search")) return <Search state={state} send={send} />;
+    if (state.matches("tickets")) return <Tickets send={send} state={state} />;
+    if (state.matches("passengers"))
+      return <Passengers state={state} send={send} />;
+    return null;
   };
 
-  return (
-    <div className='StepsLayout'>
-      {renderContent()}
-    </div>
-  );
-}; 
+  return <div className="StepsLayout">{renderContent()}</div>;
+};
